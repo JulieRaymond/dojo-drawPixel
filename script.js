472ed1003d3789d416556691c36d12865e125c7c
Julie <raymondjulie@proton.me>
2023-11-14 12:11:29 +0100

    exercice done

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 const canvasSize = 11;
+
+//STEP 1:
+const colorPicker = document.createElement("input");
+colorPicker.type = "color";
+colorPicker.id = "colorPicker";
+app.appendChild(colorPicker);
+
+//STEP 2 :
+function drawPixel() {
+  const colorSelect = colorPicker.value;
+  this.style.backgroundColor = colorSelect;
+}
+
+function resetCanvas() {
+  const cells = document.querySelectorAll("#app td");
+  cells.forEach((cell) => (cell.style.backgroundColor = ""));
+}
+
+function createCanvas() {
+  const canvasSizeinput = 11;
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+
+  for (let i = 0; i < canvasSizeinput; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < canvasSizeinput; j++) {
+      const cell = document.createElement("td");
+      cell.addEventListener("click", drawPixel);
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  }
+
+  table.appendChild(tbody);
+  app.appendChild(table);
+
+  const resetButton = document.createElement("button");
+  resetButton.textContent = "Effacer";
+  resetButton.addEventListener("click", resetCanvas);
+  app.appendChild(resetButton);
+}
+createCanvas();
